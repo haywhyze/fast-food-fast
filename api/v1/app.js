c5e2314b5c73c3e2b8d6d361ff298bc8492f61ea
Yusuf Ayo <haywhyze@gmail.com>
2018-09-06 18:51:34 +0100

    created first endpoint to get all orders

---
+++ api/v1/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+// import { request } from 'http';
+import data from './data';
+
+
+// Set up express app
+const app = express();
+
+// query to get all orders
+app.get('/orders', (request, response) => {
+  response.status(200).send({
+    success: 'true',
+    message: 'orders succesfully retrieved',
+    orders: data,
+  });
+});
+
+// Set up server
+const PORT = 3000;
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server listening on port ${PORT}`);
+});
---
+++ api/v1/data.js
@@ -0,0 +1,11 @@
+const order = [{
+  orderId: 1,
+  orderItem: {
+    pizza: 1,
+    meatPie: 2,
+    fiveAlive: 1,
+  },
+  orderStatus: 'pending',
+}];
+
+export default order;
