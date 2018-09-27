35cfcee241c25ea28f26ef0c65c54d6abbde2f8d
Yusuf Ayo <haywhyze@gmail.com>
2018-09-27 08:19:27 +0100

    ft(updateUItemplate):
    - fixed button margin on contact-us section
    - made login/signin form smaller
    - redesigned logo
    - made navbar mobile responsive
    - made navbar fixed
    - made all menu cards same size
    - made changes on checkout page
    - implemented view details on order history
    
    [FINISHES #160782009]

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,9 +1,19 @@
 const menu = document.querySelector('.header__menu');
 const icon = document.querySelector('.header__menu-icon');
+let detailsBtn = document.querySelectorAll('.details-toggle');
+
+
 
 icon.addEventListener('click', () => {
-    if (menu.classList.contains('u-show')) 
-        menu.classList.remove('u-show'); 
-    else 
-        menu.classList.add('u-show');
+    menu.classList.toggle('u-show');
+})
+
+
+detailsBtn = Array.from(detailsBtn);
+detailsBtn.map((e) => {
+    e.addEventListener('click', () => {
+        let details = e.parentNode.parentNode.nextElementSibling.firstElementChild.firstElementChild;
+        // console.log(details)
+        details.classList.toggle('u-show');
+    })
 })
