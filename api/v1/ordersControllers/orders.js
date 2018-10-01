36fd02126b373776a9170d9a322be0273aeb8ffa
Yusuf Ayo <haywhyze@gmail.com>
2018-10-01 10:30:02 +0100

    ch(restructureOrderDataModel):
    * wrote tests to test all routes.
    * changed client requent content-type to json
    * now getting just food id and quantity from the client
    * made sure the client request are filtered for invalid values
    * wrote more tests to cater for invalid requests
    [finishes #160876086]

--- api/v1/ordersControllers/orders.js
+++ api/v1/ordersControllers/orders.js
@@ -1,5 +1,13 @@
 /* eslint-disable class-methods-use-this */
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
 import data from '../data/data';
+import food from '../data/food';
+
+
+const foodIdArray = [];
+food.map(e => foodIdArray.push(e.id));
+const orderStatusArray = ['new', 'processing', 'cancelled', 'complete'];
 
 class OrdersController {
   getAllOrders(request, response) {
@@ -28,65 +36,77 @@ class OrdersController {
   }
 
   createOrder(request, response) {
-    const order = {
-      orderId: data.length + 1,
-      orderItems: [],
-      orderPrice: 0,
-      orderStatus: 'pending',
-    };
-
-    const noItemsOrdered = request.body.itemsOrdered;
-    for (let i = 0; i < Number(noItemsOrdered); i += 1) {
-      order.orderItems[i] = {};
-      order.orderItems[i].name = request.body[`orderItem_${i}_name`];
-      order.orderItems[i].quantity = request.body[`orderItem_${i}_quantity`];
-      order.orderItems[i].price = request.body[`orderItem_${i}_price`];
-      order.orderPrice += (order.orderItems[i].price * order.orderItems[i].quantity);
+    if (request.body.orderItems) {
+      request.body.orderItems.map((e) => {
+        if (Number.isNaN(Number(e.quantity))
+        || !foodIdArray.includes(Number(e.id))) {
+          return response.status(400).send({
+            success: 'false',
+            message: 'order items value not valid. Please read the docs for how to place valid order',
+          });
+        }
+      });
+      const orderItems = [];
+      let totalPrice = 0;
+      request.body.orderItems.map((e) => {
+        const index = food.findIndex(i => i.id === Number(e.id));
+        orderItems.push({
+          id: food[index].id,
+          name: food[index].name,
+          price: food[index].price,
+          quantity: e.quantity,
+        });
+        totalPrice += (e.quantity * food[index].price);
+      });
+      const order = {
+        orderId: data.length + 1,
+        orderItems,
+        orderStatus: 'new',
+        totalPrice,
+      };
+      data.push(order);
+      return response.status(201).send({
+        success: 'true',
+        message: 'order placed successfuly',
+        order,
+      });
     }
-
-    data.push(order);
-    return response.status(201).send({
-      success: 'true',
-      message: 'order placed successfuly',
-      order,
+    return response.status(400).send({
+      success: 'false',
+      message: 'order items is required',
     });
   }
+
 
   updateOrder(request, response) {
     const id = parseInt(request.params.id, 10);
     let orderFound;
     let itemIndex;
-
     data.map((order, index) => {
       if (order.orderId === id) {
         orderFound = order;
         itemIndex = index;
       }
     });
-
     if (!orderFound) {
       return response.status(404).send({
         success: 'false',
         message: 'order not found',
       });
     }
-
-    if (!request.body.orderStatus) {
+    if (!orderStatusArray.includes(request.body.orderStatus)) {
       return response.status(400).send({
         success: 'false',
-        message: 'order status is required',
+        message: 'order status must be set to specified string',
       });
     }
-
     const updatedOrder = {
       orderId: orderFound.orderId,
       orderItems: orderFound.orderItems,
       orderPrice: orderFound.orderPrice,
       orderStatus: request.body.orderStatus,
     };
-
     data.splice(itemIndex, 1, updatedOrder);
-
     return response.status(201).send({
       success: 'true',
       message: 'order updated successfully',
--- api/v1/tests/app.test.js
+++ api/v1/tests/app.test.js
@@ -1,7 +1,9 @@
+/* eslint-disable quote-props */
 import chai from 'chai';
 import { describe, it } from 'mocha';
 import app from '../app';
 
+const { expect } = chai.expect;
 
 chai.use(require('chai-http'));
 
@@ -10,8 +12,61 @@ describe('GET /orders', () => {
     chai.request(app)
       .get('/orders')
       .then((response) => {
-        chai.expect(response).to.have.status(200);
-        chai.expect(response.body).to.be.an('object');
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.an('object');
+      });
+  });
+});
+
+describe('GET /orders', () => {
+  it('should return false if wrong parameter exist', () => {
+    chai.request(app)
+      .get('/orders/tyyy')
+      .then((response) => {
+        expect(response).to.have.status(404);
+        expect(response.body.success).to.equal('false');
+      });
+  });
+});
+
+describe('GET /orders', () => {
+  it('should get a specified order', () => {
+    chai.request(app)
+      .get('/orders/1')
+      .end((error, response) => {
+        expect(response).to.have.status(200);
+        expect(response.body).to.be.an('object');
+        expect(response.body.order.orderItems[0].id).to.be.a('number');
+      });
+  });
+});
+
+describe('PUT /order', () => {
+  it('should update the order specified', () => {
+    chai.request(app)
+      .put('/orders/1')
+      .set('content-type', 'application/json')
+      .send({ 'orderStatus': 'new' })
+      .end((error, response) => {
+        expect(response).to.have.status(201);
+        expect(response.body).to.be.an('object');
+        // console.log(response.body);
+        expect(response.body.updatedOrder.orderStatus).to.equal('new');
+      });
+  });
+});
+
+describe('PUT /order', () => {
+  it('should not update the order', () => {
+    chai.request(app)
+      .put('/orders/1')
+      .set('content-type', 'application/json')
+      .send({ 'orderStatus': 'good' })
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body).to.be.an('object');
+        // console.log(response.body);
+        expect(response.body.success).to.equal('false');
       });
   });
 });
@@ -20,9 +75,26 @@ describe('POST /orders', () => {
   it('should place an order', () => {
     chai.request(app)
       .post('/orders')
-      .then((response) => {
-        chai.expect(response).to.have.status(201);
-        chai.expect(response.body).to.be.an('object');
+      .set('content-type', 'application/json')
+      .send({ 'orderItems': [{ 'id': '4302', 'quantity': '1' }] })
+      .end((error, response) => {
+        expect(response).to.have.status(201);
+        expect(response.body).to.be.an('object');
+        expect(response.body.success).to.equal('true');
+      });
+  });
+});
+
+describe('POST /orders', () => {
+  it('should not place an order', () => {
+    chai.request(app)
+      .post('/orders')
+      .set('content-type', 'application/json')
+      .send({ 'orderItems': [{ 'id': '4olo', 'quantity': 'ppp' }] })
+      .end((error, response) => {
+        expect(response).to.have.status(400);
+        expect(response.body).to.be.an('object');
+        expect(response.body.success).to.equal('false');
       });
   });
 });
